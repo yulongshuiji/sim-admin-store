a9e2dba6a0608a39f8b5865bde78224ac83b7cd4
ZXDsuv <1650490521@qq.com>
2024-06-07 19:42:42 +0800

    feat：调整表格

--- src/views/account-room/component/charts.vue
+++ src/views/account-room/component/charts.vue
@@ -29,6 +29,7 @@ export default {
           },
         },
         toolbox: {
+          show:false,
           feature: {
             dataView: { show: true, readOnly: false },
             magicType: { show: true, type: ['line', 'bar'] },
@@ -37,7 +38,8 @@ export default {
           },
         },
         legend: {
-          data: ['Evaporation', 'Precipitation', 'Temperature'],
+          // data: ['Evaporation', 'Precipitation', 'Precipitations', 'Temperature', 'Temperature2'],
+          data: []
         },
         xAxis: [
           {
@@ -51,27 +53,26 @@ export default {
         yAxis: [
           {
             type: 'value',
-            name: 'Precipitation',
             min: 0,
-            max: 250,
-            interval: 50,
+            max: 120,
+            interval: 20,
             axisLabel: {
-              formatter: '{value} ml',
+              formatter: '{value}',
             },
           },
           {
             type: 'value',
-            name: 'Temperature',
             min: 0,
-            max: 25,
-            interval: 5,
+            max: 180,
+            interval: 30,
             axisLabel: {
-              formatter: '{value} °C',
+              formatter: '{value}',
             },
           },
         ],
         series: [
           {
+            barWidth: '16px',
             name: 'Evaporation',
             type: 'bar',
             tooltip: {
@@ -79,25 +80,50 @@ export default {
                 return value + ' ml';
               },
             },
+            color: ['#81E2FF'],
             data: [
               2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3,
             ],
+            yAxisIndex: 0,
           },
           {
+            barWidth: '16px',
+
             name: 'Precipitation',
             type: 'bar',
+            color: ['#246EFF'],
+            tooltip: {
+              valueFormatter: function (value) {
+                return value + ' ml';
+              },
+            },
+            yAxisIndex: 0,
+            data: [
+              2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3,
+            ],
+          },
+          {
+            barWidth: '16px',
+
+            name: 'Precipitations',
+            type: 'bar',
+            color: ['#00B2FF'],
+            yAxisIndex: 0,
             tooltip: {
               valueFormatter: function (value) {
                 return value + ' ml';
               },
             },
             data: [
-              2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3,
+              2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 182.2, 48.7, 18.8, 6.0, 2.3,
             ],
           },
           {
             name: 'Temperature',
             type: 'line',
+            smooth: true,
+
+            color: ['#A8DB39'],
             yAxisIndex: 1,
             tooltip: {
               valueFormatter: function (value) {
@@ -105,6 +131,19 @@ export default {
               },
             },
             data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
+          },
+          {
+            name: 'Temperature2',
+            type: 'line',
+            yAxisIndex: 1,
+            smooth: true,
+            color: ['#F77E45'],
+            tooltip: {
+              valueFormatter: function (value) {
+                return value + ' °C';
+              },
+            },
+            data: [2.4, 2.5, 3.6, 4, 8.3, 15.2, 80.3, 30.4, 43.0, 66.5, 62.0, 6.2],
           },
         ],
       };
