cf7d5f44c1daf756b89ce679a107a7d376f9f986
ZXDsuv <1650490521@qq.com>
2024-06-07 15:05:01 +0800

    feat：大账房数据总览

--- src/views/account-room/component/cardList.vue
+++ src/views/account-room/component/cardList.vue
@@ -1,81 +1,223 @@
 <template>
-  <el-row :gutter="40" class="panel-group">
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+  <el-row :gutter="10" class="panel-group">
+    <el-col :xs="12" :sm="12" :lg="5" class="card-panel-col">
       <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
         <div class="card-panel-icon-wrapper icon-people">
           <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          <span>今日卖码</span>
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">
-            New Visits
+          <div class="count">
+            <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+            <span class="fix">单位：USD</span>
           </div>
-          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+          <div class="card-panel-text2">
+            <span class="text-common">较昨日</span>
+            <img :src="up" alt="">
+            <span class="up">24.8%</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">现金</span>
+            <span class="text-num">10000000</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">转账</span>
+            <span class="text-num">10000000</span>
+          </div>
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('messages')">
-        <div class="card-panel-icon-wrapper icon-message">
-          <svg-icon icon-class="message" class-name="card-panel-icon" />
+    <el-col :xs="12" :sm="12" :lg="5" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          <span>今日卖码</span>
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">
-            Messages
+          <div class="count">
+            <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+            <span class="fix">单位：USD</span>
           </div>
-          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
+          <div class="card-panel-text2">
+            <span class="text-common">较昨日</span>
+            <img :src="up" alt="">
+            <span class="up">24.8%</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">现金</span>
+            <span class="text-num">10000000</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">转账</span>
+            <span class="text-num">10000000</span>
+          </div>
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('purchases')">
-        <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="money" class-name="card-panel-icon" />
+    <el-col :xs="12" :sm="12" :lg="4" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          <span>今日卖码</span>
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">
-            Purchases
+          <div class="count">
+            <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+            <span class="fix">单位：USD</span>
           </div>
-          <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
+          <div class="card-panel-text2">
+            <span class="text-common">较昨日</span>
+            <img :src="up" alt="">
+            <span class="up">24.8%</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">现金</span>
+            <span class="text-num">10000000</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">转账</span>
+            <span class="text-num">10000000</span>
+          </div>
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
+    <el-col :xs="12" :sm="12" :lg="5" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          <span>今日卖码</span>
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">
-            Shoppings
+          <div class="count">
+            <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+            <span class="fix">单位：USD</span>
           </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+          <div class="card-panel-text2">
+            <span class="text-common">较昨日</span>
+            <img :src="up" alt="">
+            <span class="up">24.8%</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">现金</span>
+            <span class="text-num">10000000</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">转账</span>
+            <span class="text-num">10000000</span>
+          </div>
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
+    <el-col :xs="12" :sm="12" :lg="5" class="card-panel-col">
+      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+        <div class="card-panel-icon-wrapper icon-people">
+          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+          <span>今日卖码</span>
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">
-            Shoppings
+          <div class="count">
+            <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+            <span class="fix">单位：USD</span>
           </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+          <div class="card-panel-text2">
+            <span class="text-common">较昨日</span>
+            <img :src="up" alt="">
+            <span class="up">24.8%</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">现金</span>
+            <span class="text-num">10000000</span>
+          </div>
+          <div class="card-panel-text">
+            <span class="text-common">转账</span>
+            <span class="text-num">10000000</span>
+          </div>
         </div>
       </div>
     </el-col>
+
   </el-row>
 </template>
 
 <script>
 import CountTo from 'vue-count-to'
-
+import up from '@/assets/acount-room/up.svg'
+import down from '@/assets/acount-room/down.svg'
 export default {
   components: {
-    CountTo
+    CountTo,
+
+  },
+  data() {
+    return {
+      up, down,
+    }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../common.scss';
+
+.panel-group {
+  margin-top: 18px;
+
+  .card-panel-col {
+    margin-bottom: 32px;
+  }
+
+  .card-panel {
+    height: 108px;
+    cursor: pointer;
+    font-size: 12px;
+    position: relative;
+    overflow: hidden;
+
+    .card-panel-icon-wrapper {
+      float: left;
+      transition: all 0.38s ease-out;
+      border-radius: 6px;
+    }
+
+    .card-panel-icon {
+      float: left;
+      font-size: 48px;
+    }
+
+    .card-panel-description {
+      float: right;
+      font-weight: bold;
+      margin: 26px;
+      margin-left: 0px;
+
+      .card-panel-text {
+        line-height: 18px;
+        color: rgba(0, 0, 0, 0.45);
+      }
+
+      .card-panel-num {
+        font-size: 20px;
+      }
+    }
+  }
+}
+
+@media (max-width:550px) {
+  .card-panel-description {
+    display: none;
+  }
+
+  .card-panel-icon-wrapper {
+    float: none !important;
+    width: 100%;
+    height: 100%;
+    margin: 0 !important;
+
+    .svg-icon {
+      display: block;
+      margin: 14px auto !important;
+      float: none !important;
+    }
+  }
+}
+</style>
--- src/views/account-room/feat/dataView/index.vue
+++ src/views/account-room/feat/dataView/index.vue
@@ -1,37 +1,279 @@
 <template>
   <div class="dataView-c">
     <div class="header">
-      <div class="common" >
+      <div class="common">
         <img src="@/assets/acount-room/back.svg">
       </div>
-      <div class="common" >
+      <div class="common">
         <img src="@/assets/acount-room/index.svg">
       </div>
       <el-page-header class="el-h" :content="$t('accountRoom.index.shuju')">
       </el-page-header>
-      <div class="common"  style="margin-left: auto">
+      <div class="common" style="margin-left: auto">
         <i class="el-icon-refresh-right" style="color:#fff"></i>
       </div>
-
     </div>
 
     <card-list></card-list>
+
+    <el-row :gutter="24">
+      <el-col :lg="12">
+        <div class="card-list">
+          <!-- 柜台统计 -->
+          <div class="cus-card">
+            <div class="h-area">
+              <el-page-header class="el-h" :content="'柜台统计'">
+              </el-page-header>
+              <el-radio-group v-model="radio1">
+                <el-radio-button label="今日"></el-radio-button>
+                <el-radio-button label="昨日"></el-radio-button>
+                <el-radio-button label="一周"></el-radio-button>
+              </el-radio-group>
+              <span class="recent">最近统计:2024-06-01 12:35:41</span>
+              <div class="row2-area">
+                <img :src="grow2" alt="">
+              </div>
+
+            </div>
+            <div class="cus-card-content">
+              <div class="card-pri">
+                <div class="left-box">
+                  <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+                  <span class="counter">一号柜台</span>
+                </div>
+                <div class="right-box">
+                  <div class="box1">
+                    <span class="top-box">筹码</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                  <div class="box1">
+                    <span class="top-box">现金</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                </div>
+              </div>
+              <el-divider direction="vertical"></el-divider>
+              <div class="card-pri">
+                <div class="left-box">
+                  <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+                  <span class="counter">一号柜台</span>
+                </div>
+                <div class="right-box">
+                  <div class="box1">
+                    <span class="top-box">筹码</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                  <div class="box1">
+                    <span class="top-box">现金</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                </div>
+              </div>
+              <el-divider direction="vertical"></el-divider>
+              <div class="card-pri">
+                <div class="left-box">
+                  <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+                  <span class="counter">一号柜台</span>
+                </div>
+                <div class="right-box">
+                  <div class="box1">
+                    <span class="top-box">筹码</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                  <div class="box1">
+                    <span class="top-box">现金</span>
+                    <span class="bottom-box">100,1200,100</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="cus-card">
+            <div class="h-area">
+              <el-page-header class="el-h" :content="'码房统计'">
+              </el-page-header>
+              <el-radio-group v-model="radio1">
+                <el-radio-button label="今日"></el-radio-button>
+                <el-radio-button label="昨日"></el-radio-button>
+                <el-radio-button label="一周"></el-radio-button>
+              </el-radio-group>
+              <span class="recent">最近统计:2024-06-01 12:35:41</span>
+              <div class="row2-area">
+                <img :src="grow2" alt="">
+              </div>
+            </div>
+            <!-- 码房统计 -->
+            <div class="cus-card-content">
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+            </div>
+
+          </div>
+          <div class="cus-card">
+            <div class="h-area">
+              <el-page-header class="el-h" :content="'码房统计'">
+              </el-page-header>
+              <el-radio-group v-model="radio1">
+                <el-radio-button label="今日"></el-radio-button>
+                <el-radio-button label="昨日"></el-radio-button>
+                <el-radio-button label="一周"></el-radio-button>
+              </el-radio-group>
+              <span class="recent">最近统计:2024-06-01 12:35:41</span>
+              <div class="row2-area">
+                <img :src="grow2" alt="">
+              </div>
+            </div>
+            <!-- 仓库分析 -->
+            <div class="cus-card-content">
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+                <span class="box3">筹码个数 223333</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台出码</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </el-col>
+      <el-col :lg="12">
+        <div class="card-list">
+          <div class="cus-card">
+            <div class="h-area">
+              <el-page-header class="el-h" :content="'桌台分析'">
+              </el-page-header>
+              <el-radio-group v-model="radio1">
+                <el-radio-button label="今日"></el-radio-button>
+                <el-radio-button label="昨日"></el-radio-button>
+                <el-radio-button label="一周"></el-radio-button>
+              </el-radio-group>
+              <span class="recent">最近统计:2024-06-01 12:35:41</span>
+              <div class="row2-area">
+                <img :src="grow2" alt="">
+              </div>
+            </div>
+            <div class="cus-card-content">
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">客户总压</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+              <div class="box-bg">
+                <div class="chip-box">
+                  <span class="counter">桌台盈利</span>
+                  <img :src="claim" alt="">
+                </div>
+                <span>1,122</span>
+              </div>
+            </div>
+          </div>
+          <div class="cus-card">
+            <div class="h-area">
+              <el-page-header class="el-h" :content="'统计表'">
+              </el-page-header>
+              <el-radio-group v-model="radio1">
+                <el-radio-button label="今日"></el-radio-button>
+                <el-radio-button label="昨日"></el-radio-button>
+                <el-radio-button label="一周"></el-radio-button>
+              </el-radio-group>
+              <span class="recent">最近统计:2024-06-01 12:35:41</span>
+              <div class="row2-area">
+                <img :src="grow2" alt="">
+              </div>
+            </div>
+            <div class="cus-card-content">
+                <echarts-com></echarts-com>
+
+            </div>
+          </div>
+        </div>
+
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import cardList from '../../component/cardList.vue'
+import grow2 from '@/assets/acount-room/grow2.svg'
+import claim from '@/assets/acount-room/claim.svg'
+import EchartsCom from '../../component/charts.vue'
 export default {
-  components:{
-    cardList
+  components: {
+    cardList,
+    EchartsCom
+  },
+  data() {
+    return {
+      radio1: '今日',
+      grow2,
+      claim
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '../../common.scss';
+
+.el-page-header__left {
+  display: none;
+}
+
 .dataView-c {
   width: 100%;
-  height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px);
   padding: 10px 24px;
 
   .header {
