ee255c6da6f3feda71fa6507fe15d57bb72e820c
ZXDsuv <1650490521@qq.com>
2024-06-13 14:19:57 +0800

    update

--- src/components/error/index.vue
+++ src/components/error/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="error">
-    <img src="../../../../../assets/acount-room/responsity/error.svg" alt="">
-    <span>异常筹码</span>
+  <div class="error-con">
+    <div class="error">
+      <img src="@/assets/acount-room/responsity/error.svg" alt="">
+      <span>异常筹码</span>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -11,22 +14,31 @@ export default {
 }
 </script>
 
-<style>
-.error {
-  display: inline-flex;
-  padding: 8px 16px;
-  align-items: center;
-  width: 369px;
-  height: 212px;
-  border-radius: 4px;
-  background: #F54E4E;
-  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.72);
-  color: #FFF;
-  font-family: "PingFang SC";
-  font-size: 36px;
-  font-style: normal;
-  font-weight: 500;
-  flex-direction: column;
-  gap: 20px;
+<style lang="scss">
+.error-con {
+  position: fixed;
+  inset: 0;
+
+  .error {
+    display: inline-flex;
+    padding: 8px 16px;
+    align-items: center;
+    width: 369px;
+    height: 212px;
+    border-radius: 4px;
+    background: #F54E4E;
+    box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.72);
+    color: #FFF;
+    font-family: "PingFang SC";
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 500;
+    flex-direction: column;
+    gap: 20px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 }
 </style>
--- src/views/account-room/feat/reponsityData/featrue.vue
+++ src/views/account-room/feat/reponsityData/featrue.vue
@@ -6,14 +6,15 @@
       </el-page-header>
       <div class="f-content-card card">
         <el-form ref="form" :model="form" label-width="80px" v-if="step == 0">
-          <el-form-item required label="入库时间">
+          <el-form-item label="入库时间">
             <el-input style="width: 680px;" v-model="form.region" placeholder="">
             </el-input>
           </el-form-item>
           <el-form-item required label="币种" class="radio">
-            <el-radio-group v-model="radio">
-              <el-radio :label="3">USD</el-radio>
-              <el-radio :label="6">RMB</el-radio>
+            <el-radio-group v-model="form.currency_id">
+              <el-radio v-for="item in currencyList" :key="item.currency_id" :label="item.currency_id">
+                {{ item.currency_name }}
+              </el-radio>
             </el-radio-group>
           </el-form-item>
           <el-form-item required label="筹码类型" class="radio">
@@ -35,6 +36,7 @@
         <cusTip v-else :show="show" @closeModal="closeModal" @btnEvent="btnEvent"></cusTip>
       </div>
     </div>
+    <error-tip v-if="errorCode"></error-tip>
   </div>
 </template>
 
@@ -43,12 +45,16 @@ import CashList from './component/cashList.vue'
 import CodeBase from './component/base.vue'
 import recognition from './component/recognition.vue';
 import cusTip from './component/modal.vue'
+import ErrorTip from '@/components/error/index.vue'
+import { currencyList } from '@/api/currency'
+
 export default {
   components: {
     CashList,
     CodeBase,
     recognition,
-    cusTip
+    cusTip,
+    ErrorTip
   },
   data() {
     return {
@@ -57,7 +63,14 @@ export default {
       },
       step: 0,
       radio: '',
-      show: false
+      show: false,
+      errorCode: false,
+      // 币种列表
+      currencyList: [],
+      // 码种列表
+      counterList: [],
+      // 码种面额设置列表
+      counterChipList: [],
     }
   },
   methods: {
@@ -80,6 +93,8 @@ export default {
       if (this.step == 3) {
         this.show = true
       }
+
+
     },
     pre() {
       this.step -= 1
@@ -99,7 +114,27 @@ export default {
         // 退出
         this.back()
       }
-    }
+    },
+      // 获取币种列表
+      getCurrencyList() {
+      currencyList().then(res => {
+        this.currencyList = res.data
+      })
+    },
+    // 获取游戏列表
+    getCounterList(value) {
+      this.form.counter_id = undefined
+      counterList({currency_id: value}).then(res => {
+        this.counterList = res.data
+      })
+    },
+    // 获取码种详情
+    getCounterDetail(value) {
+      this.form.denomination = undefined
+      counterDetail({counter_id: value}).then(res => {
+        this.counterChipList = res.data.counter_chip
+      })
+    },
   },
   created() {
     this.serviceId = localStorage.getItem('serviceId')
@@ -109,6 +144,7 @@ export default {
       console.log("读取标签返回", data);
       // that.form.tags = data.tags
     })
+    this.getCurrencyList()
 
     // this.readTags()
   }
